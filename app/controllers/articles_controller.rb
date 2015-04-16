a34380317fe5ba731d76e8e4d2029a0889562d6e
102306075 <a24285293@gmail.com>
2015-04-16 23:51:55 +0800

    all

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,38 +1,45 @@
 class ArticlesController < ApplicationController
 	def index
+	
+	
+
 	@articles=Article.all
-	if params[:title].present?
-		@articles=@articles.where("title like ?","%#{params[:title]}%")
+	if params[:content].present?
+		@articles=@articles.where(:content =="0")
 	end
 	if params[:content].present?
-		@articles=@articles.where("content like ?","%#{params[:content]}%")
+		@articles=@articles.where(:content =="1")
 	end
-	if params[:coursetime].present?
-		@articles=@articles.where("coursetime like ?","%#{params[:coursetime]}%")
-	end
-	end
-	def show
-		@article=Article.find(params[:id])
-		
+	
+	
 	end
 	def new
 		@article=Article.new
+		
 	end
 	def create
 		@article=Article.new(article_params)
-
 		if @article.save
 			redirect_to root_path
 		else
-			render :new
-			
+			render:new
 		end
+		
 	end
-		def edit
-			@article=Article.find(params[:id])
+	def todo
+		@article=Article.find(params[:id])
+
+		
+	end
+	def done
+
+		
+	end
+	def edit
+		@article=Article.find(params[:id])
 
 			
-		end
+	end
 		def update
 			@article=Article.find(params[:id])
 			if @article.update(article_params)
@@ -42,15 +49,23 @@ class ArticlesController < ApplicationController
 			
 		end
 	end
+	def done
+		
+	end
+	def todo
+		
+	end
 	def destroy
 		@article=Article.find(params[:id])
 		@article.destroy
 		redirect_to root_path
 	end
-
 	private
 	def article_params
-		params.require(:article).permit(:title,:content,:coursetime)
+		params.require(:article).permit(:title,:content)
 		
 	end
-	end
+
+	
+end
+	
